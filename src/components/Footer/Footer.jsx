cff8a4400fe13241132cf259f6756d60e1e7d3f3
Sara-Rubaya <sararubaya4800@gmail.com>
2025-04-27 15:42:43 +0600

    added new repo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <p>
+                <small>Thank you for visiting our website</small>
+                <a href="/">Terms</a>
+                <a href="/">Privacy</a>
+                <a href="/fb">Facebook</a>
+                </p>
+        </div>
+    );
+};
+
+export default Footer;
